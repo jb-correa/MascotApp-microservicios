890babc75213335d06306302d9de636197f3db04
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-02-21 20:37:36 -0300

    Incorporacion de carpeta static y templates, pero se necesita corregirla

--- src/main/java/com/mascotapp/mascotapp/MascotappApplication.java
+++ src/main/java/com/mascotapp/mascotapp/MascotappApplication.java
@@ -2,9 +2,11 @@ package com.mascotapp.mascotapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = {"my_package.infrastructure.mongo"})
 public class MascotappApplication {
 
