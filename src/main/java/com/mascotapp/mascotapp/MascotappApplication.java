462bf94a6998f21e7e31c12fd6c0deaeaf56cbe5
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-04-18 15:42:07 -0300

    Commit del día 18-04-2023

--- src/main/java/com/mascotapp/mascotapp/MascotappApplication.java
+++ src/main/java/com/mascotapp/mascotapp/MascotappApplication.java
@@ -1,13 +1,8 @@
 package com.mascotapp.mascotapp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-@ComponentScan(basePackages = {"my_package.infrastructure.mongo"})
 public class MascotappApplication {
 
 	public static void main(String[] args) {
